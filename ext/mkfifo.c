1f2b3380f51a19cc6f5d577d4ac6c0a226ba1911
Nobuyoshi Nakada <nobu@ruby-lang.org>
2015-03-04 16:29:57 +0900

    Optional argument for mode

--- ext/mkfifo.c
+++ ext/mkfifo.c
@@ -2,26 +2,41 @@
 #include <string.h>
 #include <sys/stat.h>
 
-VALUE rb_cFile_mkfifo(VALUE, VALUE);
+VALUE rb_cFile_mkfifo(int, VALUE*, VALUE);
 
 void
 Init_mkfifo(void) {
-    rb_define_singleton_method(rb_cFile, "mkfifo", rb_cFile_mkfifo, 1);
+    rb_define_singleton_method(rb_cFile, "mkfifo", rb_cFile_mkfifo, -1);
 }
 
 /* Document-method: File::mkfifo
  *
  * call-seq:
- * mkfifo(path) → an_integer
+ * mkfifo(path[, mode]) -> an_integer
  *
  * Creates a named pipe at +path+. Raises +IOError+ if
  * the operation fails.
+ * If +mode+ is given, creates +path+ with the permission.  If it is
+ * not given or _nil_, user and group are readable and writable.
  */
 VALUE
-rb_cFile_mkfifo(VALUE self, VALUE name) {
-    FilePathValue(name);
+rb_cFile_mkfifo(int argc, VALUE *argv, VALUE self) {
+    VALUE name;
+    VALUE mode;
+    mode_t perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP;
 
-    if (mkfifo(RSTRING_PTR(name), S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP) < 0) {
+#ifdef rb_check_arity
+    rb_check_arity(argc, 1, 2);
+    name = argv[0];
+    mode = argv[1];
+#else
+    rb_scan_args(argc, argv, "11", &name, &mode);
+#endif
+
+    FilePathValue(name);
+    if (!NIL_P(mode)) perm = NUM2MODET(mode);
+
+    if (mkfifo(RSTRING_PTR(name), perm) < 0) {
         rb_raise(rb_eIOError, "Cannot create named pipe");
     }
 
